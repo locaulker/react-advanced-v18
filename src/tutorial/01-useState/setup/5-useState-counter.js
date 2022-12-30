03c5ec7f21d7fd9dcf565a8cb837c6aa05f43ce2
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-29 19:34:14 -0500

    completed useState

--- src/tutorial/01-useState/setup/5-useState-counter.js
+++ src/tutorial/01-useState/setup/5-useState-counter.js
@@ -8,6 +8,16 @@ const UseStateCounter = () => {
     setValue(0)
   }
 
+  // reference complex function
+  const delayedIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue(prevState => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
+
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -27,6 +37,16 @@ const UseStateCounter = () => {
         {/* Using an inline function */}
         <button className="btn" onClick={() => setValue(value + 1)}>
           Increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Counter With SetTimeout</h2>
+        <h1>{value}</h1>
+
+        {/* Using an inline function */}
+        <button className="btn" onClick={delayedIncrease}>
+          Delayed Increment
         </button>
       </section>
     </>
