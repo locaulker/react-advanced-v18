76c1420ab8832c2f679dd341ac1fcdadbe51ab5b
Lawrence <locaulker@gmail.com>
2022-12-31 21:51:17 -0500

    end L89 - Multiple Returns: Complex Example

--- src/tutorial/03-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/03-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,44 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
-const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+import React, { useState, useEffect } from "react"
 
-export default MultipleReturns;
+const url = "https://api.github.com/users/QuincyLarson"
+
+const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState("default user")
+
+  useEffect(() => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(user => {
+        const { login } = user
+        setUser(login)
+        setIsLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  )
+}
+
+export default MultipleReturns
