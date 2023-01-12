4362e7debb70201c93032f877ca89f6a84e86d95
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-12 17:53:59 -0500

    resume @L104

--- src/tutorial/06-useReducer/setup/index.js
+++ src/tutorial/06-useReducer/setup/index.js
@@ -2,23 +2,32 @@ import React, { useState, useReducer } from "react"
 import Modal from "./Modal"
 import { data } from "../../../data"
 // reducer function
+const reducer = (state, action) => {
+  if (action.type === "TESTING") {
+    return {
+      ...state,
+      people: data,
+      isModalOpen: true,
+      modalContent: "Item Added",
+    }
+  }
+  return state
+}
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+}
 
 const Index = () => {
   const [name, setName] = useState("")
-
-  const reducer = (state, action) => {}
-
-  const defaultState = {
-    people: [],
-    isModalOpen: false,
-    modalContent: "",
-  }
-
   const [state, dispatch] = useReducer(reducer, defaultState)
 
   const handleSubmit = e => {
     e.preventDefault()
     if (name) {
+      dispatch({ type: "TESTING" })
     } else {
     }
   }
